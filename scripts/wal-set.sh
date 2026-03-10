19c2b74fee1e361adb8702bf0e708ceb088f6b1d
Hezrai Moreno <hezraichagas@hotmail.com>
2026-03-10 12:30:04 -0300

    init

---
+++ scripts/wal-set.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+#
+
+export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
+
+pgrep -x "awww-daemon" > /dev/null || awww-daemon &
+
+IMG_PATH=$(realpath "$1")
+awww img "$IMG_PATH" --transition-type grow --transition-pos center
+
+wal -i "$IMG_PATH" -n
+
+pkill -USR2 waybar
